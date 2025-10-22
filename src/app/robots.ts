7e51f7e423eb3604d8d6822a7bf7e098e877c506
Taha <72602737+Nakataha@users.noreply.github.com>
2025-10-22 17:39:13 +0300

    Update robots function and base URL

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,15 +1,14 @@
-import { MetadataRoute } from "next";
+import type { MetadataRoute } from "next";
 
-const baseUrl = "https://mtdsoftware.com";
+const baseUrl = "https://www.mtdsoftware.com.tr";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: "*",
-        allow: "/",
-      },
-    ],
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
     sitemap: `${baseUrl}/sitemap.xml`,
+    host: baseUrl,
   };
 }
